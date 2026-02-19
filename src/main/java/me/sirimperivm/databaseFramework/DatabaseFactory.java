f6bc840393a4590fc1f7ef273bcea0521e19977e
SirImperivm <riccardo.chimento24@gmail.com>
2026-02-19 12:58:44 +0100

    refactor(database): Redefined the database create methods.

--- src/main/java/me/sirimperivm/databaseFramework/DatabaseFactory.java
+++ src/main/java/me/sirimperivm/databaseFramework/DatabaseFactory.java
@@ -4,16 +4,21 @@ import me.sirimperivm.databaseFramework.database.Database;
 import me.sirimperivm.databaseFramework.database.DatabaseConfig;
 import me.sirimperivm.databaseFramework.database.type.MySQL;
 import me.sirimperivm.databaseFramework.database.type.SQLite;
-import me.sirimperivm.databaseFramework.schema.TableNameResolver;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DatabaseFactory{
 
-    public static Database create(ExecutorService executor, DatabaseConfig config, TableNameResolver resolver) {
+    public static Database create(DatabaseConfig config) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        return create(executor, config);
+    }
+
+    public static Database create(ExecutorService executor, DatabaseConfig config) {
         return switch (config.getType()) {
-            case MYSQL -> new MySQL(executor, config, resolver);
-            default -> new SQLite(executor, config, resolver);
+            case MYSQL -> new MySQL(executor, config);
+            default -> new SQLite(executor, config);
         };
     }
 }
